110a735b47e2de45dd7411e247f3d78beaae41af
Nick Bucciarelli <nick.bucciarelli@gmail.com>
2015-03-05 10:55:21 -0500

    Added keys. S3 stuff.

--- Gemfile
+++ Gemfile
@@ -4,12 +4,14 @@ ruby '2.2.0'
 
 gem "paperclip", "~> 4.2"
 gem 'haml-rails'
+gem 'aws-sdk', '< 2.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', group: :development
 gem 'pg', group: :production
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,26 @@
 class Quote < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  aws_keys = if ['development', 'test'].include?(Rails.env)
+    "#{Rails.root}/config/aws-keys.yml"
+  else
+    {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY'],
+      secret_access_key: ENV['AWS_SECRET_KEY']
+    }
+  end
+
+  has_attached_file :image, 
+    styles: { :medium => "300x300>", :thumb => "100x100>" }, 
+    :default_url => "/images/:style/missing.png",
+    storage: :s3,
+    s3_credentials: aws_keys,
+    s3_permissions: 'authenticated-read',
+    path: "quotes/:basename.:extension",
+    s3_server_side_encryption: :aes256
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  def s3_url(style=nil, expires_in=30.minutes)
+    image.s3_object(style).url_for(:read, secure: true, expires: expires_in).to_s
+  end
 end
